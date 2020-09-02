6e1231b97573f865771d631afa91130e18266adf
jingbo <20470684+jingbojin@users.noreply.github.com>
2020-09-02 22:02:28 +1000

    Updates

---
+++ precache-manifest.27b98821b26882e1090546b91196a10f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfb7fa4b11f6b8960aafbdab0a9f554e",
+    "url": "/React-Redux-TS-Bootstrap/index.html"
+  },
+  {
+    "revision": "250a2075d09abd515814",
+    "url": "/React-Redux-TS-Bootstrap/static/css/main.24fc984f.chunk.css"
+  },
+  {
+    "revision": "b54a42f977eb44f0c599",
+    "url": "/React-Redux-TS-Bootstrap/static/js/2.9c713f60.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/React-Redux-TS-Bootstrap/static/js/2.9c713f60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "250a2075d09abd515814",
+    "url": "/React-Redux-TS-Bootstrap/static/js/main.9de1d29f.chunk.js"
+  },
+  {
+    "revision": "80cd1077b2ecc6d28bb6",
+    "url": "/React-Redux-TS-Bootstrap/static/js/runtime-main.d9c88487.js"
+  },
+  {
+    "revision": "8dad80289a1ece380591ad143b616747",
+    "url": "/React-Redux-TS-Bootstrap/static/media/logo.8dad8028.svg"
+  }
+]);
